418499c7e7b1ed9abd611a11044530d0fb996fa6
ArthurRudson <arthurrudson@gmail.com>
2024-09-02 13:59:59 -0300

    Respostas

--- Target-Sistemas/src/targetsystemas/Questao4.java
+++ Target-Sistemas/src/targetsystemas/Questao4.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package targetsystemas;
 
-/**
- *
- * @author Usuario
- */
 public class Questao4 {
-    
+    public static void main(String[] args) {
+        /*
+        4) Descubra a lógica e complete o próximo elemento:
+        a) 1, 3, 5, 7, -> 9 número anterior mais 2
+        b) 2, 4, 8, 16, 32, 64, -> 128 número anterior vezes 2
+        c) 0, 1, 4, 9, 16, 25, 36, -> 49 oitavo numero ímpar que seria 13 mais o número anterior
+        d) 4, 16, 36, 64, -> 100 proximo numero e quinto número par que seria 10 elevado a 2
+        e) 1, 1, 2, 3, 5, 8, -> 13 o proximo numero e ultimo numero mais o penultimo conhecido como a sequência de Fibonacci.
+        f) 2,10, 12, 16, 17, 18, 19, -> 200 proxímo número que começar com a letra D
+        */
+        
+        
+        
+    }
 }
--- Target-Sistemas/src/targetsystemas/Questao5.java
+++ Target-Sistemas/src/targetsystemas/Questao5.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package targetsystemas;
 
-/**
- *
- * @author Usuario
- */
 public class Questao5 {
+    /*
+    Ligue um dos interruptores e espere um pouco. Desligue e ligue um segundo interruptor.
+    Vá até a sala. A lâmpada desligada e quente corresponde ao primeiro interruptor,
+    a lâmpada acesa ao segundo e a lâmpada apagada e fria ao terceiro.
+    */
     
 }
